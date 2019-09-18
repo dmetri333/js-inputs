e7c407cefda78586710eb4bfa0685ec8840b12af
Demetri <dmetri333@gmail.com>
2019-09-18 17:48:21 -0400

    Fixed value issue

--- src/js/ColorPickerInput.js
+++ src/js/ColorPickerInput.js
@@ -8,28 +8,25 @@ class ColorPickerInput {
 		this.options = $.extend(true, {}, ColorPickerInput.DEFAULTS, this.element.dataset, typeof options == 'object' && options);
 		
 		this.$element.html(Util.supplant(this.options.templates.body, {name: this.options.name}));
+		
 		this.addColorPicker();	
 	}
 	
-	   
 	addColorPicker() {
-		
-        var colorPicker = new ColorPicker.MultiSpectral(this.$element.find('input')[0], {
-            color: this.options.color,
+	    var colorPicker = new ColorPicker.MultiSpectral(this.$element.find('input')[0], {
+            color: this.options.value,
             history: {
                 hidden: false,
                 colors: this.options.palette
             }, 
         });
-        
-       
-	}
+    }
 	
 }
 
 ColorPickerInput.DEFAULTS = {
 	name: '',
-	color: 'white',
+	value: 'white',
 	palette: ['#000000', '#ffffff', '#dd3333', '#dd9933', '#eeee22', '#81d742', '#1e73be', '#8224e3'],
 	templates: {
 		body: `<span class="colorpicker-input colorpicker-input--position-left">
